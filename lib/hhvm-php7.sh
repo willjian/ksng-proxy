931ef8ac2248feb6230dad16089f72b989821e2b
root <root@failover010.tenten.cloud>
2019-09-11 14:44:32 +0700

    update sw php

--- lib/hhvm-php7.sh
+++ lib/hhvm-php7.sh
@@ -1,20 +1,21 @@
 #!
 
 function pro_php7() {
-	 hhvm=`kusanagi pro_status $1 | grep HHVM 2>&1 > /dev/null; echo $?`
+	 hhvm=`kusanagi pro_status $1 | grep PHP-FPM 2>&1 > /dev/null; echo $?`
 	 if [ $hhvm -eq 0 ]; then
 	 	## edit nginx configuration
-	 	owner=`grep $1 /etc/kusanagi.d/profile.conf | grep DIR | cut -d '/' -f 3`
+	 	owner=`grep "/$1\"" /etc/kusanagi.d/profile.conf | grep DIR | cut -d '/' -f 3`
 	 	## disable using hhvm unix socket
-	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/hhvmd\/'$owner'.sock;*$\)/\t\t#\1/' /etc/nginx/conf.d/$1_http.conf
- 	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/hhvmd\/'$owner'.sock;*$\)/\t\t#\1/' /etc/nginx/conf.d/$1_ssl.conf
+	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php-fpm\/'$owner'.sock;*$\)/\t\t#\1/' /etc/nginx/conf.d/$1_http.conf
+ 	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php-fpm\/'$owner'.sock;*$\)/\t\t#\1/' /etc/nginx/conf.d/$1_ssl.conf
      	## enable using php7-fpm unix socket
 	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php7-fpm\/'$owner'.sock;*$\)/\t\t\1/' /etc/nginx/conf.d/$1_http.conf
 	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php7-fpm\/'$owner'.sock;*$\)/\t\t\1/' /etc/nginx/conf.d/$1_ssl.conf
+
 	 	## reload nginx configuration
 	 	systemctl reload nginx
 		## stop hhvm.owner service if there is no longer provision using it
-		/usr/src/check_hhvm_exist $owner
+		#/usr/src/check_hhvm_exist $owner
 	 else
 	 	echo "PHP7-FPM is still running"
 	 fi
@@ -22,26 +23,24 @@ function pro_php7() {
  }
 
 function pro_hhvm() {
-	 hhvm=`kusanagi pro_status $1 | grep HHVM 2>&1 > /dev/null; echo $?`
+	 hhvm=`kusanagi pro_status $1 | grep PHP-FPM 2>&1 > /dev/null; echo $?`
 	 if [ $hhvm -eq 1 ]; then
 	 	## edit nginx configuration
-	 	owner=`grep $1 /etc/kusanagi.d/profile.conf | grep DIR | cut -d '/' -f 3`
+	 	owner=`grep "/$1\"" /etc/kusanagi.d/profile.conf | grep DIR | cut -d '/' -f 3`
 	 	## disable using php7-fpm unix socket
 	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php7-fpm\/'$owner'.sock;*$\)/\t\t#\1/' /etc/nginx/conf.d/$1_http.conf
 	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php7-fpm\/'$owner'.sock;*$\)/\t\t#\1/' /etc/nginx/conf.d/$1_ssl.conf
      	## enable using hhvm unix socket
-	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/hhvmd\/'$owner'.sock;*$\)/\t\t\1/' /etc/nginx/conf.d/$1_http.conf
-	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/hhvmd\/'$owner'.sock;*$\)/\t\t\1/' /etc/nginx/conf.d/$1_ssl.conf
-    	##
-		if [ ! -f "/etc/systemd/system/hhvm.${owner}.service" ]; then
-			/usr/src/create-hhvm-ini -d ${owner} -u ${owner}
-		elif [ `systemctl is-active hhvm.${owner} | grep ^active 2>&1 > /dev/null;echo $?` -gt 0 ]; then
-			systemctl start hhvm.${owner}
-			systemctl enable hhvm.${owner}
-		fi	
+	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php-fpm\/'$owner'.sock;*$\)/\t\t\1/' /etc/nginx/conf.d/$1_http.conf
+	 	sed -i 's/^.*\(fastcgi_pass unix:\/var\/cache\/php-fpm\/'$owner'.sock;*$\)/\t\t\1/' /etc/nginx/conf.d/$1_ssl.conf
+        ##
+		if [ $(k_is_active php-fpm.${owner}) -gt 0 ]; then
+			systemctl start php-fpm.${owner}
+			systemctl enable php-fpm.${owner}
+		fi
 	 	## reload nginx configuration
 	 	systemctl reload nginx
 	 else
-	 	echo "HHVM is still running"
+	 	echo "PHP-FPM is still running"
 	 fi
 }
--- lib/pro-status.sh
+++ lib/pro-status.sh
@@ -4,22 +4,39 @@
 
 function show_pro() {
 	  if [ -n "$1" ]; then
-	  	local exist=`grep PROFILE /etc/kusanagi.d/profile.conf | grep $1  2>/dev/null`
+	  	local exist=`grep PROFILE /etc/kusanagi.d/profile.conf | grep "\"$1\""  2>/dev/null`
 	  	if [ -n "$exist" ]; then
 	  		php7=`grep '/var/cache/php7-fpm' /etc/nginx/conf.d/$1_http.conf | grep '#' 2>&1 > /dev/null;echo $?`
 			php7s=`grep '/var/cache/php7-fpm' /etc/nginx/conf.d/$1_ssl.conf | grep '#' 2>&1 > /dev/null;echo $?`
-			owner=`grep $1 /etc/kusanagi.d/profile.conf | grep DIR | cut -d '/' -f 3`
+			owner=`grep "/$1\"" /etc/kusanagi.d/profile.conf | grep DIR | cut -d '/' -f 3`
 	  		if [ $php7 -eq 1 ]; then
+				if [ 0 -eq $(k_is_active php7-fpm.$owner) ] ; then
 	      			echo "PHP7-FPM is running for $1 HTTP traffic"
+				else
+					echo "$1_http.conf is using PHP7-FPM socket but php7-fpm.$owner service is OFF"
+				fi
 	  		else
-	      			echo "HHVM is running for $1 HTTP traffic"
+				if [ 0 -eq $(k_is_active php-fpm.$owner) ] ; then
+	      			echo "PHP-FPM is running for $1 HTTP traffic"
+				else
+					echo "$1_http.conf is using PHP-FPM socket but PHP-FPM.$owner service is OFF"
+				fi
 			fi
 			if [ $php7s -eq 1 ]; then
+			    if [ 0 -eq $(k_is_active php7-fpm.$owner) ] ; then
 			        echo "PHP7-FPM is running for $1 HTTPs traffic"
+			    else
+			        echo "$1_ssl.conf is using PHP7-FPM socket but php7-fpm.$owner service is OFF"
+			    fi
 			else
-			        echo "HHVM is running for $1 HTTPs traffic"
+			    if [ 0 -eq $(k_is_active php-fpm.$owner) ] ; then
+			        echo "PHP-FPM is running for $1 HTTPs traffic"
+			    else
+			        echo "$1_ssl.conf is using PHP-FPM socket but php-fpm.$owner service is OFF"
+			    fi
 			fi
-	  		HOME=`grep /$1 /etc/kusanagi.d/profile.conf | grep KUSANAGI_DIR | cut -d'=' -f2`
+
+	  		HOME=`grep "/$1\"" /etc/kusanagi.d/profile.conf | grep KUSANAGI_DIR | cut -d'=' -f2`
 	  		echo "DocumentRoot = "$HOME  
 		else
 			echo "Provision $1 does not exist "
